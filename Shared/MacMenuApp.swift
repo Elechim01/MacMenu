cc81b37e7e25ed091c031bc20960d1f72296614a
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-06-13 23:03:09 +0200

    Initial Commit

---
+++ Shared/MacMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMenuApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MacMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
